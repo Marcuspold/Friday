f263eef9934da3c792f0d87b6dc3d41018459fa0
Opilane TTHK <opilane@tthk.ee>
2020-11-13 10:01:24 +0200

    first commit

---
+++ Friday/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Friday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fridayDirectoryFile = @"C:\Users\opilane\Friday\SampleDirectories.txt";
+            string SampleDirectory = @"C:\Users\opilane\Samples";
+
+
+            string[] includeNestedDirectories = Directory.GetDirectories(SampleDirectory, "*", SearchOption.AllDirectories);
+            ReadArray(includeNestedDirectories);
+
+            File.WriteAllLines(fridayDirectoryFile, includeNestedDirectories);
+
+           
+        }
+        public static void ReadArray(string[] array)
+        {
+            foreach(string item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
